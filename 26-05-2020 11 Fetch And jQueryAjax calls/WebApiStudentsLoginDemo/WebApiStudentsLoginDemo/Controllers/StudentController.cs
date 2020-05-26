2ac3761530bdab9d8e15c5493ad8c1a8eeeb6ad1
nirc7 <i2cs2013@gmail.com>
2020-05-26 14:26:54 +0300

    server demo

---
+++ 26-05-2020 11 Fetch And jQueryAjax calls/WebApiStudentsLoginDemo/WebApiStudentsLoginDemo/Controllers/StudentController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using WebApiStudentsLoginDemo.Models;
+
+namespace WebApiStudentsLoginDemo.Controllers
+{
+    //[EnableCors( "*", "*", "*")]
+    public class StudentController : ApiController
+    {
+        //[EnableCors( "*", "*", "*")]
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                return Ok(StudentsDB.GetAllStudents());
+            }
+            catch (Exception ex)
+            {
+                //return BadRequest(ex.Message);
+                return BadRequest("could not get all the students!\n  -- " + ex.Message);
+                //return Content(HttpStatusCode.BadRequest,ex);
+            }
+        }
+
+        //opt1 - time efficient
+        //[DisableCors]
+        public Student Get(string email, string password)
+        {
+            return StudentsDB.GetStudentByEmailAndPassword(email, password);
+        }
+               
+        //opt2 - less code
+        //public Student Get(string email, string password)
+        //{
+        //    return StudentsDB.GetAllStudents()
+        //        .SingleOrDefault(st => st.Email == email && st.Password == password);
+        //}
+
+        [Route(Name = "GetStudentById")]
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                Student res = StudentsDB.GetStudentById(id);
+                if (res == null)
+                {
+                    return Content(HttpStatusCode.NotFound, $"student with id= {id} was not found!");
+                }
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        //[DisableCors]
+        public IHttpActionResult Post([FromBody] Student val)
+        {
+            //return StudentsDB.GetStudentByEmailAndPassword(val.Email, val.Password);
+            try
+            {
+                Student res =  StudentsDB.InsertStudentToDb(val);
+                if (res ==null)
+                {
+                    return Content(HttpStatusCode.BadRequest, $"could not insert student {val.ToString()} or already exists !");
+                }
+                return Created(new Uri(Url.Link("GetStudentById" , new {id = res.ID })),res);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                Student s = StudentsDB.GetStudentById(id);
+                if (s != null)
+                {
+                    int res = StudentsDB.DeleteStudentById(id);
+                    if (res == 1)
+                    {
+                        return Ok();
+                    }
+                    return Content(HttpStatusCode.BadRequest, $"student with id {id} exsits but could not be deleted!!!");
+                }
+                return Content(HttpStatusCode.NotFound, "student with id = " + id + " was not found to delete!!!");
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        public IHttpActionResult Put(Student student2Update)
+        {
+            try
+            {
+                Student s = StudentsDB.GetStudentById(student2Update.ID);
+                if (s != null)
+                {
+                    int res = StudentsDB.UpdateStudent(student2Update);
+                    if (res == 1)
+                    {
+                        return Ok();
+                    }
+                    return Content(HttpStatusCode.NotModified, $"student with id {student2Update.ID} exsits but could not be modified!!!");
+                }
+                return Content(HttpStatusCode.NotFound, "student with id = " + student2Update.ID + " was not found to update!!!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ 26-05-2020 11 Fetch And jQueryAjax calls/WebApiStudentsLoginDemo/WebApiStudentsLoginDemo/Models/StudentsDB.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApiStudentsLoginDemo.Models
+{
+    static public class StudentsDB
+    {
+        static bool local = false;
+        static string strCon = null;
+        static string strConLocal = @"Data Source=E440\SQLEXPRESS;Initial Catalog=UsersDB;Integrated Security=True";
+        static string strConLIVEDNS = @"Data Source=185.60.170.14;Integrated Security=False;User ID=site23;Password=gB7%94jk;";
+
+        static StudentsDB()
+        {
+            if (local)
+            {
+                strCon = strConLocal;
+            }
+            else
+            {
+                strCon = strConLIVEDNS;
+            }
+        }
+
+        public static List<Student> GetAllStudents()
+        {
+            List<Student> sl = new List<Student>();
+            SqlConnection con = new SqlConnection(strCon);
+            SqlCommand comm = new SqlCommand("SELECT * FROM StudentsTB", con);
+            comm.Connection.Open();
+            SqlDataReader reader = comm.ExecuteReader();
+            while (reader.Read())
+            {
+                Student s = new Student(
+                    (int)reader["ID"],
+                    (string)reader["Name"], (string)reader["Email"], (string)reader["Password"],
+                    (int)reader["Grade"]);
+                sl.Add(s);
+            }
+            comm.Connection.Close();
+            return sl;
+        }
+
+        public static Student GetStudentByEmailAndPassword(string email, string password)
+        {
+            Student s = null;
+            SqlConnection con = new SqlConnection(strCon);
+            SqlCommand comm = new SqlCommand(
+                $" SELECT * FROM StudentsTB " +
+                $" WHERE Email='{email}' AND Password='{password}'", con);
+            comm.Connection.Open();
+            SqlDataReader reader = comm.ExecuteReader();
+            if (reader.Read())
+            {
+                s = new Student(
+                    (int)reader["ID"],
+                    (string)reader["Name"], (string)reader["Email"], (string)reader["Password"],
+                    (int)reader["Grade"]);
+            }
+            comm.Connection.Close();
+            return s;
+        }
+
+        public static Student GetStudentByEmail(string email)
+        {
+            Student s = null;
+            SqlConnection con = new SqlConnection(strCon);
+            SqlCommand comm = new SqlCommand(
+                $" SELECT * FROM StudentsTB " +
+                $" WHERE Email='{email}'", con);
+            comm.Connection.Open();
+            SqlDataReader reader = comm.ExecuteReader();
+            if (reader.Read())
+            {
+                s = new Student(
+                    (int)reader["ID"],
+                    (string)reader["Name"], (string)reader["Email"], (string)reader["Password"],
+                    (int)reader["Grade"]);
+            }
+            comm.Connection.Close();
+            return s;
+        }
+
+        public static Student InsertStudentToDb(Student val)
+        {
+            if (GetStudentByEmail(val.Email) != null) return null;
+
+            Student s = null;
+            SqlConnection con = new SqlConnection(strCon);
+            SqlCommand comm = new SqlCommand(
+                $" INSERT INTO StudentsTB(Name, Email, Password, Grade) " +
+                $" VALUES('{val.Name}', '{val.Email}', '{val.Password}', {val.Grade})", con);
+            comm.Connection.Open();
+            int res = comm.ExecuteNonQuery();
+            comm.Connection.Close();
+            if (res == 1)
+            {
+                s = GetStudentByEmail(val.Email);
+            }
+            return s;
+        }
+
+        public static Student GetStudentById(int id)
+        {
+            Student s = null;
+            SqlConnection con = new SqlConnection(strCon);
+            SqlCommand comm = new SqlCommand(
+                $" SELECT * FROM StudentsTB " +
+                $" WHERE ID={id}", con);
+            comm.Connection.Open();
+            SqlDataReader reader = comm.ExecuteReader();
+            if (reader.Read())
+            {
+                s = new Student(
+                    (int)reader["ID"],
+                    (string)reader["Name"], (string)reader["Email"], (string)reader["Password"],
+                    (int)reader["Grade"]);
+            }
+            comm.Connection.Close();
+            return s;
+        }
+        public static int DeleteStudentById(int id)
+        {
+            string strComm =
+                    $" DELETE StudentsTB " +
+                    $" WHERE ID={id}";
+
+            return ExcNonQ(strComm);
+        }
+
+        public static int UpdateStudent(Student s)
+        {
+            string strComm =
+                  $" UPDATE StudentsTB SET " +
+                  $" Name='{s.Name}' , " +
+                  $" Email='{s.Email}' , " +
+                  $" Password='{s.Password}' , " +
+                  $" Grade={s.Grade} " +
+                  $" WHERE ID={s.ID}";
+
+            return ExcNonQ(strComm);
+        }
+
+        private static int ExcNonQ(string comm2Run)
+        {
+            SqlConnection con = new SqlConnection(strCon);
+            SqlCommand comm = new SqlCommand(comm2Run, con);
+            comm.Connection.Open();
+            int res = comm.ExecuteNonQuery();
+            comm.Connection.Close();
+            return res;
+        }
+
+        public static List<Student> ExcReader(string comm2Run)
+        {
+            List<Student> sl = new List<Student>();
+            SqlConnection con = new SqlConnection(strCon);
+            SqlCommand comm = new SqlCommand(comm2Run, con);
+            comm.Connection.Open();
+            SqlDataReader reader = comm.ExecuteReader();
+            while (reader.Read())
+            {
+                Student s = new Student(
+                    (int)reader["ID"],
+                    (string)reader["Name"], (string)reader["Email"], (string)reader["Password"],
+                    (int)reader["Grade"]);
+                sl.Add(s);
+            }
+            comm.Connection.Close();
+            return sl;
+        }
+
+    }
+}
